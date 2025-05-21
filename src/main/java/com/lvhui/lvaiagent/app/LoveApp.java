6979222bd1b8e9fceb682ca2c95436aa9d789bfe
lv520 <1768453362@qq.com>
2025-05-21 10:38:49 +0800

    second commit

---
+++ src/main/java/com/lvhui/lvaiagent/app/LoveApp.java
@@ -0,0 +1,89 @@
+package com.lvhui.lvaiagent.app;
+
+import com.lvhui.lvaiagent.advisor.MyLoggerAdvisor;
+import com.lvhui.lvaiagent.advisor.ReReadingAdvisor;
+import com.lvhui.lvaiagent.chatmemory.FileBasedChatMemory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY;
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_RETRIEVE_SIZE_KEY;
+
+@Component
+@Slf4j
+public class LoveApp {
+    private final ChatClient chatClient;
+    private static final String SYSTEM_PROMPT = "你是一个恋爱专家，请根据用户输入，给出专业的恋爱建议。";
+
+    /**
+     * 初始化ai客户端
+     * @param dashscopeChatModel
+     */
+    public LoveApp(ChatModel dashscopeChatModel) {
+        // 初始化基于文件的对话记忆
+        String fileDir = System.getProperty("user.dir") + "/tmp/chat-memory";
+        ChatMemory chatMemory = new FileBasedChatMemory(fileDir);
+        // 初始化基于内存的对话记忆
+        // ChatMemory chatMemory = new InMemoryChatMemory();
+        chatClient = ChatClient.builder(dashscopeChatModel)
+                .defaultSystem(SYSTEM_PROMPT)
+                .defaultAdvisors(
+                        new MessageChatMemoryAdvisor(chatMemory),
+                        // 自定义日志advisor
+                        new MyLoggerAdvisor()
+//                        // 自定义推理增强advisor
+//                        new ReReadingAdvisor()
+                )
+                .build();
+    }
+
+
+    public String dochat(String message,String chatId){
+        ChatResponse chatResponse = chatClient
+                .prompt()
+                .user(message)
+                .advisors(spec->spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY,chatId)
+                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
+                .call()
+                .chatResponse();
+        String content = chatResponse.getResult().getOutput().getText();
+        log.info("content:{}",content);
+        return content;
+    }
+
+    /**
+     * 快速定义类
+     * @param title
+     * @param suggestions
+     */
+    record LoveReport(String title, List<String> suggestions) {
+    }
+
+    /**
+     * 结构化输出
+     * @param message
+     * @param chatId
+     * @return
+     */
+    public LoveReport doChatWithReport(String message, String chatId) {
+        LoveReport loveReport = chatClient
+                .prompt()
+                .system(SYSTEM_PROMPT + "每次对话后都要生成恋爱结果，标题为{用户名}的恋爱报告，内容为建议列表")
+                .user(message)
+                .advisors(spec -> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
+                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
+                .call()
+                .entity(LoveReport.class);
+        log.info("loveReport: {}", loveReport);
+        return loveReport;
+    }
+
+}
---
+++ src/test/java/com/lvhui/lvaiagent/app/LoveAppTest.java
@@ -0,0 +1,37 @@
+package com.lvhui.lvaiagent.app;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class LoveAppTest {
+    @Resource
+    private LoveApp loveApp;
+    @Test
+    void dochat() {
+        String chatId = UUID.randomUUID().toString();
+        String message = "你好，我是程序员 小辉";
+        String answer = loveApp.dochat(message,chatId);
+        message = "我喜欢 小美，我该怎么办";
+        answer = loveApp.dochat(message,chatId);
+        Assertions.assertNotNull(answer);
+        message = "我喜欢的是谁来着，告诉我她的名字叫什么";
+        answer = loveApp.dochat(message,chatId);
+        Assertions.assertNotNull(answer);
+    }
+
+    @Test
+    void doChatWithReport() {
+        String chatId = UUID.randomUUID().toString();
+        // 第一轮
+        String message = "你好，我是程序员小辉，我想让另一半（编程导航）更爱我，但我不知道该怎么做";
+        LoveApp.LoveReport loveReport = loveApp.doChatWithReport(message, chatId);
+        Assertions.assertNotNull(loveReport);
+    }
+
+}
