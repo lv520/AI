b9225c5805459aaa6cb26099edf87ec7d3a078e8
lv520 <1768453362@qq.com>
2025-05-22 21:56:49 +0800

    Sixth commit

--- src/main/java/com/lvhui/lvaiagent/app/LoveApp.java
+++ src/main/java/com/lvhui/lvaiagent/app/LoveApp.java
@@ -16,6 +16,7 @@ import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.tool.ToolCallback;
+import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Component;
 
@@ -161,5 +162,31 @@ public class LoveApp {
         log.info("content: {}", content);
         return content;
     }
+
+    @Resource
+    private ToolCallbackProvider toolCallbackProvider;
+
+    /**
+     * 调用mcp服务
+     * @param message
+     * @param chatId
+     * @return
+     */
+    public String doChatWithMcp(String message, String chatId) {
+        ChatResponse response = chatClient
+                .prompt()
+                .user(message)
+                .advisors(spec -> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
+                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
+                // 开启日志，便于观察效果
+                .advisors(new MyLoggerAdvisor())
+                .tools(toolCallbackProvider)
+                .call()
+                .chatResponse();
+        String content = response.getResult().getOutput().getText();
+        log.info("content: {}", content);
+        return content;
+    }
+
 
 }
--- src/test/java/com/lvhui/lvaiagent/app/LoveAppTest.java
+++ src/test/java/com/lvhui/lvaiagent/app/LoveAppTest.java
@@ -70,4 +70,17 @@ class LoveAppTest {
         Assertions.assertNotNull(answer);
     }
 
+    @Test
+    void doChatWithMcp() {
+        String chatId = UUID.randomUUID().toString();
+        // 测试地图 MCP
+//        String message = "我的另一半居住在上海静安区，请帮我找到 5 公里内合适的约会地点";
+//        String answer =  loveApp.doChatWithMcp(message, chatId);
+//        Assertions.assertNotNull(answer);
+
+        String message = "需要一些猫的图片";
+        String answer =  loveApp.doChatWithMcp(message, chatId);
+        Assertions.assertNotNull(answer);
+    }
+
 }
